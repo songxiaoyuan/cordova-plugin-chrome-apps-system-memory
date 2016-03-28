20943fb8de6eacf0505dd59f31b1d00f951761fe
xiaoyuan <xiaoyuan@nfs.iscas.ac.cn>
2016-03-28 16:22:01 +0800

    add import

--- src/android/ChromeSystemMemory.java
+++ src/android/ChromeSystemMemory.java
@@ -4,6 +4,13 @@
 
 package org.chromium;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStream;  
+import java.io.InputStreamReader;  
+import java.net.MalformedURLException;  
+import java.net.URL; 
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaInterface;
